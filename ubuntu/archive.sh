36e0e1cdf49d8c3a73ea14246b5bf72a213bb65b
liyumin <chn.lee.yumi@gmail.com>
2020-10-18 17:52:59 +0800

    ubuntu取消同步i386的一些文件

--- ubuntu/archive.sh
+++ ubuntu/archive.sh
@@ -22,17 +22,17 @@ if [ ! -d ${BASEDIR} ]; then
   mkdir -p ${BASEDIR} || fatal "Creation of ${BASEDIR} failed."
 fi
 
-# 参考debian的ftpsync脚本得到ubuntu需要exclude的路径
+# 参考debian的ftpsync脚本得到ubuntu需要exclude的路径（TODO：exclude好像有点问题，*_all.deb不会删除）
 rsync --recursive --times --links --safe-links --hard-links \
   --stats \
   --exclude "Packages*" --exclude "Sources*" \
   --exclude "Release*" --exclude "InRelease" \
-  --exclude='/dists/**/binary-i386/' --exclude='/dists/**/installer-i386/' --exclude='/dists/**/Contents-i386.gz' --exclude='/pool/**/*_i386.deb' --exclude='/pool/**/*_i386.udeb' \
+  --exclude='/dists/**/binary-i386/' --exclude='/dists/**/installer-i386/' --exclude='/dists/**/Contents-i386.gz' --exclude='/pool/**/*_i386.deb' --exclude='/pool/**/*_i386.udeb' --exclude='/pool/**/*_all.deb' --exclude='/dists/**/*-i386.yml.gz' --exclude='/dists/**/*-i386.yml.xz' --exclude='/dists/**/*-i386.json.gz' --exclude='/dists/**/*-i386.xz' \
   ${RSYNCSOURCE} ${BASEDIR} || fatal "First stage of sync failed."
 
 rsync --recursive --times --links --safe-links --hard-links \
   --stats --delete --delete-after \
-  --exclude='/dists/**/binary-i386/' --exclude='/dists/**/installer-i386/' --exclude='/dists/**/Contents-i386.gz' --exclude='/pool/**/*_i386.deb' --exclude='/pool/**/*_i386.udeb' \
+  --exclude='/dists/**/binary-i386/' --exclude='/dists/**/installer-i386/' --exclude='/dists/**/Contents-i386.gz' --exclude='/pool/**/*_i386.deb' --exclude='/pool/**/*_i386.udeb' --exclude='/pool/**/*_all.deb' --exclude='/dists/**/*-i386.yml.gz' --exclude='/dists/**/*-i386.yml.xz' --exclude='/dists/**/*-i386.json.gz' --exclude='/dists/**/*-i386.xz' \
   ${RSYNCSOURCE} ${BASEDIR} || fatal "Second stage of sync failed."
 
 date -u > ${BASEDIR}/project/trace/$(hostname -f)
